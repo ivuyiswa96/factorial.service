22aeb67a09621722e1d5d90dac31a23caeae25bc
Andiswa Ndlovu <Andiswa.Ndlovu@multichoice.co.za>
2025-07-14 01:52:22 +0200

    initial commit

---
+++ factorial.service/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numbers = { 4, 3,1 };
+
+foreach (int n in numbers)
+{
+    Thread tread = new Thread(() =>
+    {
+        long fact = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.WriteLine($"Factorial of is {fact}" );
+    });
+    tread.Start();
+}
+Thread.Sleep(1000);
